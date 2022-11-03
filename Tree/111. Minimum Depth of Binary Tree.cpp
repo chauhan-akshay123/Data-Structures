123f371f0e4bee836e4cd53905fe60be6a4bffe6
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-11-03 22:31:41 +0530

    Create 111. Minimum Depth of Binary Tree.cpp

---
+++ Tree/111. Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,17 @@
+int minDepth(TreeNode* root) {
+        if(root==NULL){
+          return 0;  
+        }    
+        if(root->left==NULL && root->right==NULL){
+            return 1;
+        }
+        int l = INT_MAX,r=INT_MAX;
+        if(root->left){
+            l = minDepth(root->left); 
+        }
+        if(root->right){
+            r = minDepth(root->right); 
+        }
+        
+        return min(l,r)+1;
+    }
